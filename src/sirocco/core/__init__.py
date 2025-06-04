5faab61a673bb77846937954fefa93fc02705b68
Alexander Goscinski <alexander.goscinski@posteo.de>
2025-06-04 19:28:29 +0200

    Support for copy of local scripts to remote machine
    Support the copy of local scripts to remote machine by adding the script
    specified in `src` to the nodes of the `ShellJob`. Because aiida does
    not copy the file over preserving the execution permission, we cannot
    support a syntax of the form `./icon.py`. Therefore I changed the usage
    to the syntax "python icon.py".
    
    Introduce localhost enum to only expand env variables when localhost. I
    think this will be removed as the support of environment variables is
    quite fragile since it opens the door for multiple valid uses cases that
    are hard to support at the same time.

--- src/sirocco/core/__init__.py
+++ src/sirocco/core/__init__.py
@@ -1,5 +1,18 @@
+from sirocco.core import enums
+
 from ._tasks import IconTask, ShellTask
 from .graph_items import AvailableData, Cycle, Data, GeneratedData, GraphItem, Task
 from .workflow import Workflow
 
-__all__ = ["Workflow", "GraphItem", "Data", "AvailableData", "GeneratedData", "Task", "Cycle", "ShellTask", "IconTask"]
+__all__ = [
+    "enums",
+    "Workflow",
+    "GraphItem",
+    "Data",
+    "AvailableData",
+    "GeneratedData",
+    "Task",
+    "Cycle",
+    "ShellTask",
+    "IconTask",
+]
---
+++ src/sirocco/core/enums.py
@@ -0,0 +1,7 @@
+import enum
+
+
+class Computer(enum.Enum):
+    """Enum for labeling computer hostnames."""
+
+    LOCALHOST = "localhost"
