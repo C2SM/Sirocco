5c6874679fb65e48943ccc2ec901f09b2a9ae99f
Julian Geiger <julian.geiger@gmx.net>
2025-06-12 06:35:14 +0200

    use hatch run verdi in setup.sh

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -11,22 +11,22 @@ sed -i "s|PLACEHOLDER_WORK_DIR|${GITHUB_WORKSPACE}|" "${CONFIG}/localhost.yaml"
 sed -i "s|PLACEHOLDER_REMOTE_ABS_PATH_DOUBLER|${CONFIG}/doubler.sh|" "${CONFIG}/doubler.yaml"
 sed -i "s|PLACEHOLDER_SSH_KEY|${HOME}/.ssh/slurm_rsa|" "${CONFIG}/slurm-ssh-config.yaml"
 
-verdi setup --non-interactive --config "${CONFIG}/profile.yaml"
+hatch run verdi setup --non-interactive --config "${CONFIG}/profile.yaml"
 
 # set up localhost computer
-verdi computer setup --non-interactive --config "${CONFIG}/localhost.yaml"
-verdi computer configure core.local localhost --config "${CONFIG}/localhost-config.yaml"
-verdi computer test localhost
-verdi code create core.code.installed --non-interactive --config "${CONFIG}/doubler.yaml"
-verdi code create core.code.installed --non-interactive --config "${CONFIG}/add.yaml"
-verdi code create core.code.containerized --non-interactive --config "${CONFIG}/add-containerized.yaml"
+hatch run verdi computer setup --non-interactive --config "${CONFIG}/localhost.yaml"
+hatch run verdi computer configure core.local localhost --config "${CONFIG}/localhost-config.yaml"
+hatch run verdi computer test localhost
+hatch run verdi code create core.code.installed --non-interactive --config "${CONFIG}/doubler.yaml"
+hatch run verdi code create core.code.installed --non-interactive --config "${CONFIG}/add.yaml"
+hatch run verdi code create core.code.containerized --non-interactive --config "${CONFIG}/add-containerized.yaml"
 
 # set up slurm-ssh computer
-verdi computer setup --non-interactive --config "${CONFIG}/slurm-ssh.yaml"
-verdi computer configure core.ssh slurm-ssh --non-interactive --config "${CONFIG}/slurm-ssh-config.yaml" -n  # needs slurm container
-verdi computer test slurm-ssh --print-traceback
+hatch run verdi computer setup --non-interactive --config "${CONFIG}/slurm-ssh.yaml"
+hatch run verdi computer configure core.ssh slurm-ssh --non-interactive --config "${CONFIG}/slurm-ssh-config.yaml" -n  # needs slurm container
+hatch run verdi computer test slurm-ssh --print-traceback
 
-verdi profile setdefault test_aiida
-verdi config set runner.poll.interval 0
-verdi config set warnings.development_version False
-verdi config set warnings.rabbitmq_version False
+hatch run verdi profile setdefault test_aiida
+hatch run verdi config set runner.poll.interval 0
+hatch run verdi config set warnings.development_version False
+hatch run verdi config set warnings.rabbitmq_version False
