93651d526c1a35241cbe30ba4f33c3086f7941dc
leclairm <mattthieu.leclair@c2sm.ethz.ch>
2025-11-17 15:43:28 +0100

    fix[install]: unquote command with uenv run

--- launch_install_santis.sh
+++ launch_install_santis.sh
@@ -2,7 +2,7 @@
 
 BUILD_DIR="/dev/shm/${USER}/sirocco"
 mkdir -p "${BUILD_DIR}"
-uenv run --view default /capstor/store/cscs/userlab/cwd01/leclairm/uenvs/images/sirocco_25.9.sqfs -- "${PWD}/install_santis.sh ${BUILD_DIR}"
+uenv run --view default /capstor/store/cscs/userlab/cwd01/leclairm/uenvs/images/sirocco_25.9.sqfs -- ${PWD}/install_santis.sh ${BUILD_DIR}
 # srun --uenv /capstor/store/cscs/userlab/cwd01/leclairm/uenvs/images/sirocco_25.9.sqfs --view default $PWD/install_santis.sh ${1}
 
 
