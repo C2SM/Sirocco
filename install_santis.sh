9a259e2f32d6148413b4285b31c7027ef5c1038a
leclairm <mattthieu.leclair@c2sm.ethz.ch>
2025-11-26 09:07:47 +0100

    upd: new uenv

--- install_santis.sh
+++ install_santis.sh
@@ -14,8 +14,8 @@ fi
 uv venv --relocatable --python="$(which python)" --prompt="༄ sirocco ༄"
 source .venv/bin/activate
 CC=$(which gcc) \
-CFLAGS="-I/user-environment/env/default/include -I/user-environment/env/default/include/graphviz" \
-LDFLAGS="-L/user-environment/env/default/lib -L/user-environment/env/default/lib/graphviz" \
+CFLAGS="-I/user-environment/env/sirocco/include -I/user-environment/env/sirocco/include/graphviz" \
+LDFLAGS="-L/user-environment/env/sirocco/lib -L/user-environment/env/sirocco/lib/graphviz" \
 uv sync --no-cache --link-mode=copy --compile-bytecode --active --no-editable --inexact || exit
 
 mksquashfs ${VENV} ${VENV_SQUASHFS} -no-recovery -noappend -Xcompression-level 3 || exit
