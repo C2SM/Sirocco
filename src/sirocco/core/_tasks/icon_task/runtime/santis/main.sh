5509e7677e04ea8a3d4c7274c81af53a08e09c20
leclairm <mattthieu.leclair@c2sm.ethz.ch>
2026-06-01 07:45:58 +0200

    ref:icon:: minor changes to main icon entry point

--- src/sirocco/core/_tasks/icon_task/runtime/santis/main.sh
+++ src/sirocco/core/_tasks/icon_task/runtime/santis/main.sh
@@ -1,16 +1,24 @@
-#!/usr/local/bin/bash -l
+#!/usr/local/bin/bash
 
-# Dump SLURM environment variables
+set -e
+
+# Dump environment
+# ----------------
+# Dump SLURM environment variables to stdout
 set | grep SLURM
+# Dump full environment to file
+set > ./env_${SLURM_JOB_ID}
 
 # Generate SLURM hostfile for arbitrary task distribution
+# -------------------------------------------------------
 SIROCCO_HOSTFILE="./hostfile-sirocco"
 export SLURM_HOSTFILE="./hostfile-${SLURM_JOB_ID}"
 export SLURM_HOSTFILE_ANNOTATED="./hostfile-${SLURM_JOB_ID}_annotated"
 ./generate_hostfile.sh ${SIROCCO_HOSTFILE} ${SLURM_HOSTFILE} ${SLURM_HOSTFILE_ANNOTATED}
 
-# build srun command
-srun_cmd="srun --ntasks=${N_PROCS} --hint=nomultithread --distribution=arbitrary"
+# Build srun command
+# ------------------
+srun_cmd="srun -l --kill-on-bad-exit=1 --mpi=cray_shasta --ntasks=${N_PROCS} --hint=nomultithread --distribution=arbitrary"
 [ -n "${CORES_PER_PROC}" ] && srun_cmd+=" --cpus-per-task=${CORES_PER_PROC}"
 [ -n "${SIROCCO_UENV}" ] && srun_cmd+=" --uenv=${SIROCCO_UENV}"
 [ -n "${SIROCCO_VIEW}" ] && srun_cmd+=" --view=${SIROCCO_VIEW}"
@@ -25,6 +33,12 @@ else
     exit 1
 fi
 
-# launch
+# Launch
+# ------
 echo "running ICON with ${srun_cmd}"
 ${srun_cmd}
+
+# Accounting
+# ----------
+echo " ==> Accounting"
+sacct -j "${SLURM_JOB_ID}" --format "JobID, JobName, AllocCPUs, Elapsed, ElapsedRaw, CPUTimeRAW, ConsumedEnergyRaw, MaxRSS, MaxVMSize, AveRSS"
