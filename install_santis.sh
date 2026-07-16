d06d5d59987027349e2a839b960895b61a69f976
leclairm <mattthieu.leclair@c2sm.ethz.ch>
2026-07-16 17:14:49 +0200

    fix: install on /dev/shm

--- install_santis.sh
+++ install_santis.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/bash -l
 
-BUILD_DIR="${BUILD_DIR:-/dev/shm/$USER/sirocco_install}"
+build_dir="/dev/shm/$USER/sirocco_install"
 squash_name="sirocco_venv.squashfs"
 
-pushd "${BUILD_DIR}" >/dev/null 2>&1
-
-uv venv --relocatable --python="$(which python)" --prompt="༄ sirocco ༄"
-source .venv/bin/activate
+mkdir -p ${build_dir}/.venv
+uv venv --clear --relocatable --python="$(which python)" --prompt="༄ sirocco ༄" ${build_dir}/.venv
+source ${build_dir}/.venv/bin/activate
 uv sync --no-cache --link-mode=copy --compile-bytecode --active --no-editable --inexact || exit
 
-mksquashfs .venv "${squash_name}" -no-recovery -noappend -Xcompression-level 3 || exit
+mksquashfs ${build_dir}/.venv ./"${squash_name}" -no-recovery -noappend -Xcompression-level 3 || exit
 
-popd >/dev/null 2>&1
-
-rsync -av "${BUILD_DIR}/${squash_name}" .
+rm -rf ${build_dir}
