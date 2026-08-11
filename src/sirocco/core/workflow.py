a549fd0c199dc5f3d5f3e848358a0af25f9484f5
leclairm <mattthieu.leclair@c2sm.ethz.ch>
2026-08-11 18:41:28 +0200

    add: colored circles corresponding to action

--- src/sirocco/core/workflow.py
+++ src/sirocco/core/workflow.py
@@ -319,7 +319,7 @@ class Workflow:
                 task.rank = 0
                 self.scheduler.submit(task)
                 self.front[0].append(task)
-                msg = f"{task.label} ({task.jobid}) SUBMITTED to rank {task.rank}"
+                msg = f"🔵 {task.label} ({task.jobid}) SUBMITTED to rank {task.rank}"
                 logger.info(msg)
         for k in range(self.front_depth - 1):
             for task in self.front[k]:
@@ -328,7 +328,7 @@ class Workflow:
                         self.scheduler.submit(child)
                         child.rank = k + 1
                         self.front[k + 1].append(child)
-                        msg = f"{child.label} ({child.jobid}) SUBMITTED to rank {child.rank}"
+                        msg = f"🔵 {child.label} ({child.jobid}) SUBMITTED to rank {child.rank}"
                         logger.info(msg)
 
     def restart_front(self, logger: logging.Logger) -> None:
@@ -344,12 +344,12 @@ class Workflow:
                     in (TaskStatus.COMPLETED, TaskStatus.RUNNING, TaskStatus.WAITING)
                 ):
                     self.cool_down_tasks.remove(task)
-                    msg = f"{task.label} ({task.jobid}) CONTINUED as rank 0 from cool-down"
+                    msg = f"🔵 {task.label} ({task.jobid}) CONTINUED as rank 0 from cool-down"
                     logger.info(msg)
                 else:
                     self.scheduler.cancel(task)
                     self.scheduler.submit(task)
-                    msg = f"{task.label} ({task.jobid}) SUBMITTED to rank {task.rank}"
+                    msg = f"🔵 {task.label} ({task.jobid}) SUBMITTED to rank {task.rank}"
                     logger.info(msg)
 
     def propagate_front(self, logger: logging.Logger) -> None:
@@ -359,7 +359,7 @@ class Workflow:
         to_promote: list[Task] = []
         for task in self.front[0]:
             if (status := self.scheduler.get_status(task)) == TaskStatus.FAILED:
-                msg = f"{task.label} ({task.jobid}) FAILED"
+                msg = f"🔴 {task.label} ({task.jobid}) FAILED"
                 logger.info(msg)
                 self.cancel_all_tasks(mode="cancel", logger=logger)
                 msg = f"All workflow tasks canceled because {task.label} failed"
@@ -372,7 +372,7 @@ class Workflow:
             task.rank = -1
             self.front[0].remove(task)
             self.completed_tasks.append(task)
-            msg = f"{task.label} ({task.jobid}) COMPLETED"
+            msg = f"🟢 {task.label} ({task.jobid}) COMPLETED"
             logger.info(msg)
 
         # Update front rank of tasks currently in the front after the first generation
@@ -381,8 +381,8 @@ class Workflow:
             for task in to_promote:
                 task.rank = k - 1
                 self.front[k].remove(task)
+                msg = f"🔵 {task.label} ({task.jobid}) PROMOTED from rank {k} to {k - 1}"
                 self.front[k - 1].append(task)
-                msg = f"{task.label} ({task.jobid}) PROMOTED from rank {k} to {k - 1}"
                 logger.info(msg)
 
         # Add new tasks to the last generation of the front
@@ -396,7 +396,7 @@ class Workflow:
                     self.scheduler.submit(child)
                     child.rank = self.front_depth - 1
                     self.front[-1].append(child)
-                    msg = f"{child.label} ({child.jobid}) SUBMITTED to rank {child.rank}"
+                    msg = f"🔵 {child.label} ({child.jobid}) SUBMITTED to rank {child.rank}"
                     logger.info(msg)
 
     def cancel_all_tasks(self, mode: Literal["cancel", "cool-down"], logger: logging.Logger) -> None:
@@ -410,11 +410,11 @@ class Workflow:
                     and self.scheduler.get_status(task) in (TaskStatus.COMPLETED, TaskStatus.RUNNING)
                 ):
                     self.cool_down_tasks.append(task)
-                    msg = f"{task.label} ({task.jobid}) COOLING DOWN"
+                    msg = f"🔵 {task.label} ({task.jobid}) COOLING DOWN"
                     logger.info(msg)
                 else:
                     self.scheduler.cancel(task)
-                    msg = f"{task.label} ({task.jobid}) CANCELED"
+                    msg = f"🔴 {task.label} ({task.jobid}) CANCELED"
                     logger.info(msg)
 
     def auto_submit(self) -> None:
