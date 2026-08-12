51a304f439e465383cd71e0477028f9004fa6fbf
leclairm <mattthieu.leclair@c2sm.ethz.ch>
2026-08-12 16:44:36 +0200

    fix: missing some vars for the icon4py environment

--- src/sirocco/core/_tasks/icon_task/runtime/santis/santis_environments.sh
+++ src/sirocco/core/_tasks/icon_task/runtime/santis/santis_environments.sh
@@ -79,6 +79,14 @@ santis_icon4py_environment(){
     export GT4PY_UNSTRUCTURED_HORIZONTAL_HAS_UNIT_STRIDE=1
     export DACE_compiler_cuda_block_size_limit=256
     export PY2FGEN_LOG_LEVEL=WARNING
+
+    export PMI_MMAP_SYNC_WAIT_TIME=300
+    export HWMALLOC_LARGE_LIMIT=$((1 << 26)) # 64 MiB, default 2 MiB
+    export HWMALLOC_LARGE_SEGMENT_SIZE=$((1 << 26)) # 64 MiB, default 2 MiB
+    export HWMALLOC_NEVER_FREE=1 # This should make sure that even if halos are bigger than the above options, the allocations are still kept around
+    export MPICH_GPU_IPC_CACHE_MAX_SIZE=100 # default 50, shouldn't really make a difference, but you never know
+    export NV_ACC_CUDA_MEMALLOCASYNC=1
+    export NV_ACC_CUDA_MEMALLOCASYNC_POOLSIZE=500000000000
 } 
 
 # IO environment
